83f9b7ed1aaf356e39f103b550bac757b24ce16d
dmitry.malofeev <dim2k2006@yandex.ru>
2022-02-02 21:26:00 +0100

    Add "countValue" function

--- src/sprint-1/a-function-values/index.js
+++ src/sprint-1/a-function-values/index.js
@@ -5,3 +5,26 @@ const countValue = (a, x, b, c) => {
 };
 
 module.exports = countValue;
+
+// Yandex context required code
+// const _readline = require('readline');
+//
+// const _reader = _readline.createInterface({
+//   input: process.stdin,
+// });
+//
+// const _inputLines = [];
+//
+// _reader.on('line', (line) => {
+//   _inputLines.push(line);
+// });
+//
+// const solve = () => {
+//   const values = _inputLines[0].split(' ').map(Number);
+//
+//   const result = countValue(...values);
+//
+//   console.log(result);
+// };
+//
+// process.stdin.on('end', solve);
