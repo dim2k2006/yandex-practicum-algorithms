a4f9b2c295152f057bdbb0538e7719c731f2385e
dmitry.malofeev <dim2k2006@yandex.ru>
2022-02-06 10:28:06 +0100

    Update "c-neighbors"

--- src/sprint-1/c-neighbors/index.js
+++ src/sprint-1/c-neighbors/index.js
@@ -30,26 +30,34 @@ const getNeighbors = (matrix, point) => {
 module.exports = getNeighbors;
 
 // Yandex context required code
-// const _readline = require('readline');
-//
-// const _reader = _readline.createInterface({
-//   input: process.stdin,
-// });
-//
-// const _inputLines = [];
-//
-// _reader.on('line', (line) => {
-//   _inputLines.push(line);
-// });
-//
-// const solve = () => {
-//   const values = _inputLines[0].split(' ').map(Number);
-//
-//   const result = main(...values);
-//
-//   const out = result ? 'WIN' : 'FAIL';
-//
-//   console.log(out);
-// };
-//
-// process.stdin.on('end', solve);
+const _readline = require('readline');
+
+const _reader = _readline.createInterface({
+  input: process.stdin,
+});
+
+const _inputLines = [];
+
+_reader.on('line', (line) => {
+  _inputLines.push(line);
+});
+
+const solve = () => {
+  const matrixRowsCount = Number(_inputLines[0]);
+  // const matrixColumnsCount = Number(_inputLines[1]);
+  const matrix = [...new Array(matrixRowsCount)].map((value, index) => {
+    const matrixRow = _inputLines[index + 2].split(' ').map(Number);
+
+    return matrixRow;
+  });
+  const pointY = Number(_inputLines[_inputLines.length - 2]);
+  const pointX = Number(_inputLines[_inputLines.length - 1]);
+
+  const neighbors = getNeighbors(matrix, { y: pointY, x: pointX });
+
+  const result = neighbors.join(' ');
+
+  console.log(result);
+};
+
+process.stdin.on('end', solve);
