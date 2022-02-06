1cfa3ccd6eadfa1d1c729048157482057881575d
dmitry.malofeev <dim2k2006@yandex.ru>
2022-02-06 10:31:34 +0100

    Update "c-neighbors"

--- src/sprint-1/c-neighbors/index.js
+++ src/sprint-1/c-neighbors/index.js
@@ -22,7 +22,7 @@ const getNeighbors = (matrix, point) => {
       return value;
     })
     .filter((value) => value !== undefined)
-    .sort();
+    .sort((a, b) => a - b);
 
   return neighbors;
 };
@@ -30,34 +30,34 @@ const getNeighbors = (matrix, point) => {
 module.exports = getNeighbors;
 
 // Yandex context required code
-const _readline = require('readline');
-
-const _reader = _readline.createInterface({
-  input: process.stdin,
-});
-
-const _inputLines = [];
-
-_reader.on('line', (line) => {
-  _inputLines.push(line);
-});
-
-const solve = () => {
-  const matrixRowsCount = Number(_inputLines[0]);
-  // const matrixColumnsCount = Number(_inputLines[1]);
-  const matrix = [...new Array(matrixRowsCount)].map((value, index) => {
-    const matrixRow = _inputLines[index + 2].split(' ').map(Number);
-
-    return matrixRow;
-  });
-  const pointY = Number(_inputLines[_inputLines.length - 2]);
-  const pointX = Number(_inputLines[_inputLines.length - 1]);
-
-  const neighbors = getNeighbors(matrix, { y: pointY, x: pointX });
-
-  const result = neighbors.join(' ');
-
-  console.log(result);
-};
-
-process.stdin.on('end', solve);
+// const _readline = require('readline');
+//
+// const _reader = _readline.createInterface({
+//   input: process.stdin,
+// });
+//
+// const _inputLines = [];
+//
+// _reader.on('line', (line) => {
+//   _inputLines.push(line);
+// });
+//
+// const solve = () => {
+//   const matrixRowsCount = Number(_inputLines[0]);
+//   // const matrixColumnsCount = Number(_inputLines[1]);
+//   const matrix = [...new Array(matrixRowsCount)].map((value, index) => {
+//     const matrixRow = _inputLines[index + 2].split(' ').map(Number);
+//
+//     return matrixRow;
+//   });
+//   const pointY = Number(_inputLines[_inputLines.length - 2]);
+//   const pointX = Number(_inputLines[_inputLines.length - 1]);
+//
+//   const neighbors = getNeighbors(matrix, { y: pointY, x: pointX });
+//
+//   const result = neighbors.join(' ');
+//
+//   console.log(result);
+// };
+//
+// process.stdin.on('end', solve);
--- src/sprint-1/c-neighbors/index.test.js
+++ src/sprint-1/c-neighbors/index.test.js
@@ -23,3 +23,15 @@ test('Returns [0, 2].', () => {
 
   expect(getNeighbors(matrix, point)).toEqual([0, 2]);
 });
+
+test('Returns [0, 2].', () => {
+  const matrix = [
+    [1, 2, 3],
+    [0, 2, 6],
+    [7, 4, 1],
+    [2, 7, 0],
+  ];
+  const point = { y: 0, x: 0 };
+
+  expect(getNeighbors(matrix, point)).toEqual([0, 2]);
+});
