989a69589a3c82208b98debb7bed46c250fa556e
dmitry.malofeev <dim2k2006@yandex.ru>
2022-02-07 19:53:32 +0100

    Update

--- src/sprint-1-final/a-nearest-zero/index.js
+++ src/sprint-1-final/a-nearest-zero/index.js
@@ -6,34 +6,48 @@
 //   return index + 1;
 // };
 
-const findDistance = (data, currentIndex) => {
-  let leftPointer = currentIndex;
-  let rightPointer = currentIndex;
-  let i = 0;
-  let result = 0;
+// const findDistance = (data, currentIndex) => {
+//   let leftPointer = currentIndex;
+//   let rightPointer = currentIndex;
+//   let i = 0;
+//   let result = 0;
+//
+//   while (i < data.length) {
+//     const leftValue = data[leftPointer];
+//     const rightValue = data[rightPointer];
+//
+//     if (leftValue === 0) return currentIndex - leftPointer;
+//
+//     if (rightValue === 0) return rightPointer - currentIndex;
+//
+//     leftPointer = Math.max(0, leftPointer - 1);
+//     rightPointer = Math.min(rightPointer + 1, data.length - 1);
+//
+//     i = i + 1;
+//   }
+//
+//   return result;
+// };
 
-  while (i < data.length) {
-    const leftValue = data[leftPointer];
-    const rightValue = data[rightPointer];
+const findDistance = (currentIndex, zeroIndexes) => {
+  const distances = zeroIndexes
+    .map((index) => Math.abs(currentIndex - index))
+    .sort((a, b) => a - b);
 
-    if (leftValue === 0) return currentIndex - leftPointer;
-
-    if (rightValue === 0) return rightPointer - currentIndex;
-
-    leftPointer = Math.max(0, leftPointer - 1);
-    rightPointer = Math.min(rightPointer + 1, data.length - 1);
-
-    i = i + 1;
-  }
-
-  return result;
+  return distances[0];
 };
 
 const getNearestZero = (data) => {
+  const zeroIndexes = data.reduce((accumulator, item, index) => {
+    if (item !== 0) return accumulator;
+
+    return [...accumulator, index];
+  }, []);
+
   const result = data.map((number, index) => {
     if (number === 0) return 0;
 
-    const distance = findDistance(data, index);
+    const distance = findDistance(index, zeroIndexes);
 
     return distance;
   });
@@ -44,24 +58,24 @@ const getNearestZero = (data) => {
 module.exports = getNearestZero;
 
 // Yandex context required code
-// const _readline = require('readline');
-//
-// const _reader = _readline.createInterface({
-//   input: process.stdin,
-// });
-//
-// const _inputLines = [];
-//
-// _reader.on('line', (line) => {
-//   _inputLines.push(line);
-// });
-//
-// const solve = () => {
-//   const data = _inputLines[1].split(' ').map(Number);
-//
-//   const result = getNearestZero(data).join(' ');
-//
-//   console.log(result);
-// };
-//
-// process.stdin.on('end', solve);
+const _readline = require('readline');
+
+const _reader = _readline.createInterface({
+  input: process.stdin,
+});
+
+const _inputLines = [];
+
+_reader.on('line', (line) => {
+  _inputLines.push(line);
+});
+
+const solve = () => {
+  const data = _inputLines[1].split(' ').map(Number);
+
+  const result = getNearestZero(data).join(' ');
+
+  console.log(result);
+};
+
+process.stdin.on('end', solve);
