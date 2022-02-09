383519750a5acc8ef023f2a4d57f8b3470f3a4e2
dmitry.malofeev <dim2k2006@yandex.ru>
2022-02-09 20:43:30 +0100

    Update

--- src/sprint-1-final/a-nearest-zero/index.js
+++ src/sprint-1-final/a-nearest-zero/index.js
@@ -1,15 +1,18 @@
 const getNearestZero = (data) => {
   let prevZeroIndex = null;
 
+  let nextIndex = data.indexOf(0, 1);
+  let nextZeroIndex = nextIndex === -1 ? Number.MAX_SAFE_INTEGER : nextIndex;
+
   const result = data.map((number, index) => {
     if (number === 0) {
       prevZeroIndex = index;
 
+      nextIndex = data.indexOf(0, index + 1);
+      nextZeroIndex = nextIndex === -1 ? Number.MAX_SAFE_INTEGER : nextIndex;
+
       return 0;
     }
-
-    const nextIndex = data.indexOf(0, index + 1);
-    const nextZeroIndex = nextIndex === -1 ? Number.MAX_SAFE_INTEGER : nextIndex;
 
     const leftDistance =
       prevZeroIndex === null ? Number.MAX_SAFE_INTEGER : index - prevZeroIndex;
@@ -26,24 +29,24 @@ const getNearestZero = (data) => {
 module.exports = getNearestZero;
 
 // Yandex context required code
-// const _readline = require('readline');
-//
-// const _reader = _readline.createInterface({
-//   input: process.stdin,
-// });
-//
-// const _inputLines = [];
-//
-// _reader.on('line', (line) => {
-//   _inputLines.push(line);
-// });
-//
-// const solve = () => {
-//   const data = _inputLines[1].split(' ').map(Number);
-//
-//   const result = getNearestZero(data).join(' ');
-//
-//   console.log(result);
-// };
-//
-// process.stdin.on('end', solve);
+const _readline = require('readline');
+
+const _reader = _readline.createInterface({
+  input: process.stdin,
+});
+
+const _inputLines = [];
+
+_reader.on('line', (line) => {
+  _inputLines.push(line);
+});
+
+const solve = () => {
+  const data = _inputLines[1].split(' ').map(Number);
+
+  const result = getNearestZero(data).join(' ');
+
+  console.log(result);
+};
+
+process.stdin.on('end', solve);
