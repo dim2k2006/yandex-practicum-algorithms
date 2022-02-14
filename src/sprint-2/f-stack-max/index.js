21ea3c43df8e6cdc893352f0ff36d04b6923f2eb
dmitry.malofeev <dim2k2006@yandex.ru>
2022-02-14 18:36:07 +0100

    Add "f-stack-max"

---
+++ src/sprint-2/f-stack-max/index.js
@@ -0,0 +1,56 @@
+class StackMax {
+  constructor() {
+    this.stack = [];
+  }
+
+  push(x) {
+    this.stack.push(x);
+  }
+
+  pop() {
+    return this.stack.pop();
+  }
+
+  get_max() {
+    if (this.stack.length === 0) return 'None';
+
+    const result = this.stack.reduce((accumulator, item) => {
+      return Math.max(accumulator, item);
+    }, Number.MIN_SAFE_INTEGER);
+
+    return result;
+  }
+}
+
+const executeStackCommand = (stack, commandName, ...args) => {
+  const result = stack[commandName](...args);
+
+  return result;
+};
+
+exports.StackMax = StackMax;
+exports.executeStackCommand = executeStackCommand;
+
+// Yandex context required code
+// const _readline = require('readline');
+//
+// const _reader = _readline.createInterface({
+//   input: process.stdin,
+// });
+//
+// const _inputLines = [];
+//
+// _reader.on('line', (line) => {
+//   _inputLines.push(line);
+// });
+//
+// const solve = () => {
+//   const string1 = _inputLines[0];
+//   const string2 = _inputLines[1];
+//
+//   const result = getExtraLetter(string1, string2);
+//
+//   console.log(result);
+// };
+//
+// process.stdin.on('end', solve);
---
+++ src/sprint-2/f-stack-max/index.test.js
@@ -0,0 +1,29 @@
+const { StackMax, executeStackCommand } = require('./');
+
+describe('get_max command.', () => {
+  test('Returns None.', () => {
+    const stack = new StackMax();
+
+    expect(executeStackCommand(stack, 'get_max')).toBe('None');
+  });
+
+  test('Returns 1.', () => {
+    const stack = new StackMax();
+
+    stack.push(1);
+
+    expect(executeStackCommand(stack, 'get_max')).toBe(1);
+  });
+
+  test('Returns 100.', () => {
+    const stack = new StackMax();
+
+    stack.push(50);
+    stack.push(99);
+    stack.push(100);
+    stack.push(-50);
+    stack.push(0);
+
+    expect(executeStackCommand(stack, 'get_max')).toBe(100);
+  });
+});
