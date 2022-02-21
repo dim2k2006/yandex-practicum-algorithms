f6046282d50122ab3b742f44896088d765c9b7b8
dmitry.malofeev <dim2k2006@yandex.ru>
2022-02-21 17:44:15 +0100

    Update "a-deque"

--- src/sprint-2-final/a-deque/index.test.js
+++ src/sprint-2-final/a-deque/index.test.js
@@ -27,7 +27,7 @@ describe('size method.', () => {
   });
 });
 
-describe('push method.', () => {
+describe('push_back method.', () => {
   test('Returns error if max size is exceeded.', () => {
     const deque = new Deque(0);
 
@@ -47,45 +47,23 @@ describe('push method.', () => {
   });
 });
 
-// describe('pop method.', () => {
-//   test('Returns None if queue is empty.', () => {
-//     const queue = new MyQueueSized(5);
-//
-//     expect(queue.pop()).toBe('None');
-//   });
-//
-//   test('Removes and returns elements from queue.', () => {
-//     const queue = new MyQueueSized(5);
-//
-//     queue.push(1);
-//     queue.push(2);
-//     queue.push(3);
-//
-//     const result = queue.pop();
-//
-//     expect(result).toBe(1);
-//     expect(queue.size()).toBe(2);
-//   });
-// });
-//
-// describe('peek method.', () => {
-//   test('Prints None if queue is empty.', () => {
-//     const queue = new MyQueueSized(5);
-//
-//     expect(queue.peek()).toBe('None');
-//   });
-//
-//   test('Prints first queue item.', () => {
-//     const queue = new MyQueueSized(5);
-//
-//     queue.push(1);
-//     queue.push(2);
-//     queue.push(3);
-//
-//     expect(queue.peek()).toBe(1);
-//
-//     queue.pop();
-//
-//     expect(queue.peek()).toBe(2);
-//   });
-// });
+describe('pop_front method.', () => {
+  test('Returns None if queue is empty.', () => {
+    const deque = new Deque(5);
+
+    expect(deque.pop_front()).toBe('None');
+  });
+
+  test('Removes and returns elements from queue.', () => {
+    const deque = new Deque(5);
+
+    deque.push_back(1);
+    deque.push_back(2);
+    deque.push_back(3);
+
+    const result = deque.pop_front();
+
+    expect(result).toBe(1);
+    expect(deque.size()).toBe(2);
+  });
+});
