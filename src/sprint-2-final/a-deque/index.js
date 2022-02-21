2fede2b57367ff39a04ca19221c06762c0879563
dmitry.malofeev <dim2k2006@yandex.ru>
2022-02-21 17:41:36 +0100

    Add "a-deque"

---
+++ src/sprint-2-final/a-deque/index.js
@@ -0,0 +1,91 @@
+class Deque {
+  constructor(maxSize) {
+    this.maxSize = maxSize;
+    this.queue = [...new Array(maxSize)].map(() => null);
+    this.head = 0;
+    this.tail = 0;
+    this.sizeValue = 0;
+  }
+
+  push_back(x) {
+    if (this.size() === this.maxSize) return 'error';
+
+    this.queue[this.tail] = x;
+    this.tail = (this.tail + 1) % this.maxSize;
+    this.sizeValue = this.sizeValue + 1;
+
+    return undefined;
+  }
+
+  pop_front() {
+    if (this.size() === 0) return 'None';
+
+    const result = this.queue[this.head];
+
+    this.queue[this.head] = null;
+    this.head = (this.head + 1) % this.maxSize;
+    this.sizeValue = this.sizeValue - 1;
+
+    return result;
+  }
+
+  size() {
+    return this.sizeValue;
+  }
+}
+
+exports.Deque = Deque;
+
+// Yandex context required code
+// const executeStackCommand = (queue, commandName, ...args) => {
+//   const result = queue[commandName](...args);
+//
+//   if (commandName === 'push' && result === 'error') {
+//     console.log(result);
+//
+//     return;
+//   }
+//
+//   if (commandName === 'pop' && result === 'None') {
+//     console.log(result);
+//
+//     return;
+//   }
+//
+//   if (result !== undefined) {
+//     console.log(result);
+//   }
+// };
+//
+// const _readline = require('readline');
+//
+// const _reader = _readline.createInterface({
+//   input: process.stdin,
+// });
+//
+// const _inputLines = [];
+//
+// _reader.on('line', (line) => {
+//   _inputLines.push(line);
+// });
+//
+// const solve = () => {
+//   const commandsCount = Number(_inputLines[0]);
+//   const queueMaxSize = Number(_inputLines[1]);
+//   const commands = [...new Array(commandsCount)].map((item, index) => {
+//     const commandString = _inputLines[index + 2];
+//     const [commandName, commandValue] = commandString.split(' ');
+//
+//     return [commandName, commandValue ? Number(commandValue) : undefined];
+//   });
+//
+//   const queue = new MyQueueSized(queueMaxSize);
+//
+//   commands.forEach((commandList) => {
+//     const [commandName, commandValue] = commandList;
+//
+//     executeStackCommand(queue, commandName, commandValue);
+//   });
+// };
+//
+// process.stdin.on('end', solve);
---
+++ src/sprint-2-final/a-deque/index.test.js
@@ -0,0 +1,91 @@
+const { Deque } = require('./');
+
+describe('size method.', () => {
+  test('Returns 0.', () => {
+    const deque = new Deque(5);
+
+    expect(deque.size()).toBe(0);
+  });
+
+  test('Returns 0.', () => {
+    const deque = new Deque(5);
+
+    deque.push_back(1);
+    deque.push_back(2);
+    deque.push_back(3);
+
+    expect(deque.size()).toBe(3);
+
+    deque.pop_front();
+    deque.pop_front();
+
+    expect(deque.size()).toBe(1);
+
+    deque.pop_front();
+
+    expect(deque.size()).toBe(0);
+  });
+});
+
+// describe('push method.', () => {
+//   test('Returns error if max size is exceeded.', () => {
+//     const queue = new MyQueueSized(0);
+//
+//     const result = queue.push(1);
+//
+//     expect(result).toBe('error');
+//   });
+//
+//   test('Pushes data to the queue.', () => {
+//     const queue = new MyQueueSized(5);
+//
+//     queue.push(1);
+//     queue.push(2);
+//     queue.push(3);
+//
+//     expect(queue.size()).toBe(3);
+//   });
+// });
+//
+// describe('pop method.', () => {
+//   test('Returns None if queue is empty.', () => {
+//     const queue = new MyQueueSized(5);
+//
+//     expect(queue.pop()).toBe('None');
+//   });
+//
+//   test('Removes and returns elements from queue.', () => {
+//     const queue = new MyQueueSized(5);
+//
+//     queue.push(1);
+//     queue.push(2);
+//     queue.push(3);
+//
+//     const result = queue.pop();
+//
+//     expect(result).toBe(1);
+//     expect(queue.size()).toBe(2);
+//   });
+// });
+//
+// describe('peek method.', () => {
+//   test('Prints None if queue is empty.', () => {
+//     const queue = new MyQueueSized(5);
+//
+//     expect(queue.peek()).toBe('None');
+//   });
+//
+//   test('Prints first queue item.', () => {
+//     const queue = new MyQueueSized(5);
+//
+//     queue.push(1);
+//     queue.push(2);
+//     queue.push(3);
+//
+//     expect(queue.peek()).toBe(1);
+//
+//     queue.pop();
+//
+//     expect(queue.peek()).toBe(2);
+//   });
+// });
