d43f73640f611540e34b505597c18096bf70101d
dmitry.malofeev <dim2k2006@yandex.ru>
2022-03-14 20:23:08 +0100

    Update "n-flower-beds"

--- src/sprint-3/n-flower-beds/index.js
+++ src/sprint-3/n-flower-beds/index.js
@@ -1,40 +1,66 @@
-const isPairExistsInAccumulator = (pair, accumulator) => {
-  const result = accumulator.find((item) => item[0] >= pair[0] && item[1] <= pair[1]);
-
-  return result;
-};
+// const isPairExistsInAccumulator = (pair, accumulator) => {
+//   const result = accumulator.find((item) => item[0] >= pair[0] && item[1] <= pair[1]);
+//
+//   return result;
+// };
+//
+// const getBorders = (coords) => {
+//   const iter = (list, accumulator) => {
+//     console.log('list:', list);
+//     console.log('accumulator:', accumulator);
+//
+//     if (list.length === 0) return accumulator;
+//
+//     const pair = list[0];
+//
+//     if (accumulator.length === 0) return iter(list.slice(1), [pair]);
+//
+//     // const isExistsInAccumulator = isPairExistsInAccumulator(pair, accumulator);
+//     //
+//     // if (isExistsInAccumulator) return iter(list.slice(1), accumulator);
+//
+//     const newAccumulator = accumulator.map((item) => {
+//       if (pair[0] <= item[0] && pair[1] >= item[1]) {
+//         return pair;
+//       }
+//
+//       return item;
+//     });
+//
+//     return iter(list.slice(1), newAccumulator);
+//   };
+//
+//   const result = iter(coords, []);
+//
+//   console.log('result:', result);
+//
+//   return result;
+// };
 
 const getBorders = (coords) => {
+  const uniqueCoords = new Map();
+
+  coords.forEach((item) => uniqueCoords.set(item.join(''), item));
+
+  const iterator = uniqueCoords.values();
+
   const iter = (list, accumulator) => {
-    console.log('list:', list);
-    console.log('accumulator:', accumulator);
+    const listItem = list.next().value;
 
-    if (list.length === 0) return accumulator;
+    if (listItem === undefined) return accumulator;
 
-    const pair = list[0];
+    if (accumulator.length === 0) return iter(list, [...accumulator, listItem]);
 
-    if (accumulator.length === 0) return iter(list.slice(1), [pair]);
+    const filteredAccumulator = accumulator.filter(
+      (item) => !(item[0] >= listItem[0] && item[1] <= listItem[1]),
+    );
 
-    // const isExistsInAccumulator = isPairExistsInAccumulator(pair, accumulator);
-    //
-    // if (isExistsInAccumulator) return iter(list.slice(1), accumulator);
+    const newAccumulator = [...filteredAccumulator, listItem];
 
-    const newAccumulator = accumulator.map((item) => {
-      if (pair[0] <= item[0] && pair[1] >= item[1]) {
-        return pair;
-      }
-
-      return item;
-    });
-
-    return iter(list.slice(1), newAccumulator);
+    return iter(list, newAccumulator);
   };
 
-  const result = iter(coords, []);
-
-  console.log('result:', result);
-
-  return result;
+  return iter(iterator, []);
 };
 
 exports.getBorders = getBorders;
